55ba81dca486c611895a8584d8b4a09da9a52b15
praveenkumar <jvpraveen03@gmail.com>
2026-09-17 22:08:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -91,11 +91,12 @@ def upload():
 
         file_data = video.read()
 
-        supabase.storage.from_(BUCKET_NAME).upload(
-    filename,
-    file_data,
+       supabase.storage.from_(BUCKET_NAME).upload(
+    path=filename,
+    file=file_data,
     file_options={
-        "content-type": video.content_type
+        "content-type": video.content_type,
+        "upsert": "true"
     }
 )
 
